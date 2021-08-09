1ebb9b7cee59a54b9ca95c838eafbbf4fa9d62e8
SatyamAgrawal <satyam0499@gmail.com>
2021-08-09 18:55:07 +0530

    update hevm interface

--- contracts/test.sol
+++ contracts/test.sol
@@ -11,9 +11,16 @@ interface IERC20Like {
 
 interface Hevm {
 
-    function warp(uint256) external;
-
-    function store(address,bytes32,bytes32) external;
+    // Sets block timestamp to `x`
+    function warp(uint256 x) external view;
+    // Sets slot `loc` of contract `c` to value `val`
+    function store(address c, bytes32 loc, bytes32 val) external view;
+    // Reads the slot `loc` of contract `c`
+    function load(address c, bytes32 loc) external view returns (bytes32 val);
+    // Generates address derived from private key `sk`
+    function addr(uint256 sk) external view returns (address _addr);
+    // Signs `digest` with private key `sk` (WARNING: this is insecure as it leaks the private key)
+    function sign(uint256 sk, bytes32 digest) external view returns (uint8 v, bytes32 r, bytes32 s);
 
 }
 
