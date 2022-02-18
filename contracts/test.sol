81af7a404bb0aa87b1955804565262589de63927
Lucas Manuel <lucas@maple.finance>
2022-02-18 09:22:55 -0700

    feat: update constrictToRange to be inclusive

--- contracts/test.sol
+++ contracts/test.sol
@@ -67,8 +67,28 @@ contract TestUtils is DSTest {
         fail();
     }
 
-    function constrictToRange(uint256 input, uint256 min, uint256 max) internal pure returns (uint256 output) {
-        return min == max ? max : input % (max - min) + min;
+    // Constrict values to a range, inclusive of min and max values. Source: solmate
+    function constrictToRange(
+        uint256 x,
+        uint256 min,
+        uint256 max
+    ) internal pure returns (uint256 result) {
+        require(max >= min, "MAX_LESS_THAN_MIN");
+
+        uint256 size = max - min;
+
+        if (max != type(uint256).max) size++; // Make the max inclusive.
+        if (size == 0) return min;            // Using max would be equivalent as well.
+
+        // Ensure max is inclusive in cases where x != 0 and max is at uint max.
+        if (max == type(uint256).max && x != 0) x--; // Accounted for later.
+
+        if (x < min) x += size * (((min - x) / size) + 1);
+
+        result = min + ((x - min) % size);
+
+        // Account for decrementing x to make max inclusive.
+        if (max == type(uint256).max && x != 0) result++;
     }
 
     // Manipulate mainnet ERC20 balance
