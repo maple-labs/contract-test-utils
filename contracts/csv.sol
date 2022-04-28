908a9ac352048aa554fe7923e0d04447f0e53562
Erick <erick@maple.finance>
2022-04-28 13:59:47 -0400

    chore: organize

--- contracts/csv.sol
+++ contracts/csv.sol
@@ -9,10 +9,37 @@ abstract contract CSVWriter {
 
     mapping (string => string[][]) csvs;
 
-    function initCSV(string memory filePath_, string[] memory header_) internal {
+    /*************************/
+    /*** Storage Functions ***/
+    /*************************/
+
+    function init(string memory filePath_, string[] memory header_) internal {
         string[][] storage csv = csvs[filePath_] = new string[][](0);
         csv[0] = header_;
     }
+
+    function addCell(string memory filePath_, uint256 row_, string memory content_) internal {
+        string[][] storage csv = csvs[filePath_];
+        csv[row_].push(content_);
+    }
+
+    function addRow(string memory filePath_, string[] memory row_) internal {
+        string[][] storage csv = csvs[filePath_];
+        csv.push(row_);
+    }
+
+    function modifyCell(string memory filePath_, uint256 row_, uint256 column_, string memory content_) internal {
+        string[][] storage csv = csvs[filePath_];
+        csv[row_][column_] = content_;
+    }
+
+    function clearCSV(string memory filePath_) internal {
+        delete csvs[filePath_];
+    }
+
+    /**********************/
+    /*** File Functions ***/
+    /**********************/
 
     function writeCSV(string memory filePath_) internal {
         string[][] storage csv = csvs[filePath_];
@@ -21,11 +48,7 @@ abstract contract CSVWriter {
         }
     }
 
-    function clearCSV(string memory filePath_) internal {
-        delete csvs[filePath_];
-    }
-
-    function deleteFile(string memory filePath_) internal {
+    function deleteCSV(string memory filePath_) internal {
         string[] memory inputs = new string[](3);
         inputs[0] = "scripts/rm-file.sh";
         inputs[1] = "-f";
@@ -33,6 +56,21 @@ abstract contract CSVWriter {
 
         vm.ffi(inputs);
     }
+
+    function compareCSV(string memory filePath_A_, string memory filePath_B_) internal {
+        string[] memory inputs = new string[](5);
+        inputs[0] = "scripts/cmp-files.sh";
+        inputs[1] = "-a";
+        inputs[2] = filePath_A_;
+        inputs[3] = "-b";
+        inputs[4] = filePath_B_;
+
+        vm.ffi(inputs);
+    }
+
+    /************************/
+    /*** Helper Functions ***/
+    /************************/
 
     function writeLine(string memory filePath_, uint256 index_) private {
         string[][] storage csv = csvs[filePath_];
